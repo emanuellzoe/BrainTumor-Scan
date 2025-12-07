fb34c9bde2e0be71bc60dc8fe5b8a2038b433b7d
Emmanuel Zoe <emanuellzoee@gmail.com>
2025-12-07 22:01:43 +0700

    oke sudah ada header dan footer

---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+// app/components/Header.tsx
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#222', color: 'white', padding: '1rem', textAlign: 'center' }}>
+      <h1>Brain Tumor Detection</h1>
+    </header>
+  );
+};
+
+export default Header;
